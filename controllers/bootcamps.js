08e7fefe3fa15e0a386632504bf20d99e260959d
chache <daniel2chache@gmail.com>
2021-08-06 20:07:01 +0300

    bootcamp and course api done

---
+++ controllers/bootcamps.js
@@ -0,0 +1,138 @@
+const path = require("path");
+const ErrorResponse = require("../utils/errorResponse");
+const asyncHandler = require("../middleware/async");
+const geocoder = require("../utils/geocoder");
+const Bootcamp = require("../models/Bootcamp");
+//@desc     Get all bootcamps
+//@route    GET /api/v1/bootcamps
+//@access   Public
+exports.getBootcamps = asyncHandler(async (req, res, next) => {
+  res.status(200).json(res.advancedResults);
+});
+
+//@desc     Get single bootcamps
+//@route    GET /api/v1/bootcamps/:id
+//@access   Public
+exports.getBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findById(req.params.id);
+
+  // error if id not in db
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
+  }
+
+  res.status(200).json({ success: true, data: bootcamp });
+});
+
+//@desc     create new bootcamp
+//@route    POST /api/v1/bootcamps
+//@access   Private
+exports.createBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.create(req.body);
+  res
+    .status(201)
+    .json({ success: true, msg: "Create new bootcamp", data: bootcamp });
+});
+
+//@desc     Update bootcamps
+//@route    PUT /api/v1/bootcamps/:id
+//@access   Private
+exports.updateBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
+  }
+  res.status(200).json({ success: true, data: bootcamp });
+});
+
+//@desc     Delete bootcamps
+//@route    DELETE /api/v1/bootcamps/:id
+//@access   Private
+exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findById(req.params.id);
+
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
+  }
+  bootcamp.remove();
+  res.status(200).json({ success: true, data: {} });
+});
+
+//@desc     Get Bootcamps within a radius
+//@route    GET /api/v1/bootcamps/radius/:zipcode /:distance
+//@access   Private
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+  const { zipcode, distance } = req.params;
+
+  //Get lat/lng from geocoder
+  const loc = await geocoder.geocode(zipcode);
+  const lat = loc[0].latitude;
+  const lng = loc[0].longitude;
+
+  //calculate radius using radians
+  //Divide distance by radius of  Earth
+  //Earth Radius = 3,963 mi or 6,378km we will use miles
+  const radius = distance / 3963;
+
+  const bootcamps = await Bootcamp.find({
+    location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+  });
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    data: bootcamps,
+  });
+});
+
+//@desc     upload photo for bootcamp
+//@route    PUT /api/v1/bootcamps/:id/photo
+//@access   Private
+exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findById(req.params.id);
+
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
+  }
+  if (!req.files) {
+    return next(new ErrorResponse(`Please upload a file`, 400));
+  }
+  const file = req.files.file;
+  // Make sure the image is a photo
+  if (!file.mimetype.startsWith("image")) {
+    return next(new ErrorResponse(`Please upload an image file`, 400));
+  }
+  //Check file size
+  if (file.size > process.env.MAX_FILE_UPLOAD) {
+    return next(
+      new ErrorResponse(
+        `Please upload an image file less than ${process.env.MAX_FILE_UPLOAD}`,
+        400
+      )
+    );
+  }
+  // create custom file name
+  file.name = `photo_${bootcamp._id}${path.parse(file.name).ext}`;
+  file.mv(`${process.env.FILE_UPLOAD_PATH}/${file.name}`, async (err) => {
+    if (err) {
+      console.error(err);
+      return next(new ErrorResponse(`Problem with file upload`, 500));
+    }
+    await Bootcamp.findByIdAndUpdate(req.params.id, { photo: file.name });
+    res.status(200).json({
+      success: true,
+      data: file.name,
+    });
+  });
+});
